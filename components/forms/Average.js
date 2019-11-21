fcd683993294226df19b5e8f5e9443380beb9f5d
Ryan <ryan.betts@student.swic.edu>
2019-11-20 19:44:26 -0600

    fixed forms for proper input

--- components/forms/Average.js
+++ components/forms/Average.js
@@ -1,24 +1,8 @@
 export default function() {
   return `
 
-
 <form>
-<div>
-  <label for="lastName">Last Name</label>
-  <input type="text" id="lastName" />
-</div>
-
-<div>
-  <label for="studentID">Student ID</label>
-  <input type="number" id="studentID" />
-</div>
-
-<div>
-  <label for="score">Score</label>
-  <input type="number" id="score" />
-</div>
-
-  <input type="submit" value="Add Test Score!" />
+<input type="submit" value="Add Test Score!" />
 </form>
 `;
 }
--- components/forms/TallyGrades.js
+++ components/forms/TallyGrades.js
@@ -2,21 +2,6 @@ export default function() {
   return `
 
 <form>
-<div>
-  <label for="lastName">Last Name</label>
-  <input type="text" id="lastName" />
-</div>
-
-<div>
-  <label for="studentID">Student ID</label>
-  <input type="number" id="studentID" />
-</div>
-
-<div>
-  <label for="score">Score</label>
-  <input type="number" id="score" />
-</div>
-
   <input type="submit" value="Add Test Score!" />
 </form>
 `;
