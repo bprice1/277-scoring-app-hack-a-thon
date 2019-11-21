b6b9ba5831fe20ae5fd9d1f00cd413df4b832d4d
Ryan <ryan.betts@student.swic.edu>
2019-11-20 19:24:51 -0600

    removed hard coded form and imported other forms for the forms dir

--- components/Main.js
+++ components/Main.js
@@ -1,5 +1,7 @@
 import * as  from "./components";
 
+import * as  from "./components/forms";
+
 export default () =>
   `
   <main>
@@ -9,23 +11,5 @@ export default () =>
             <button id="getAverage">Average</button>
             <button id="tallyGrades">Tally</button>
         </div>
-      <form>
-        <div>
-          <label for="lastName">Last Name</label>
-          <input type="text" id="lastName" />
-        </div>
-
-        <div>
-          <label for="studentID">Student ID</label>
-          <input type="number" id="studentID" />
-        </div>
-
-        <div>
-          <label for="score">Score</label>
-          <input type="number" id="score" />
-        </div>
-
-          <input type="submit" value="Add Test Score!" />
-      </form>
     </main>
 `;
