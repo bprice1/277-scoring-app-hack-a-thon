961ac9c5c6ee167b7a893b777b7d91d68d40d81c
Ryan <ryan.betts@student.swic.edu>
2019-11-20 18:55:11 -0600

    add render function

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function() {
+  return `
+<footer>
+      <p>&bull;Fexmon &bull;All rights reserved.</p>
+</footer>
+`;
+}
---
+++ components/Header.js
@@ -0,0 +1,14 @@
+export default function() {
+  return `
+<header>
+      <figure>
+        <img
+          srcset="https://picsum.photos/400 2x, https://picsum.photos/200 1x"
+          src="https://picsom.photos/200"
+          alt="Some Random Image 2"
+        />
+      </figure>
+      <h1>Fexmon</h1>
+    </header>
+    `;
+}
